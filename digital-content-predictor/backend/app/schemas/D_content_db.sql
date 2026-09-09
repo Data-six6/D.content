2d22f949f11a1be2730d9cf3a0f0538c39dfe6ac
Pendragonnnnn <chansothymonthai01@gmail.com>
2026-09-09 19:45:39 +0700

    update database schema

--- digital-content-predictor/backend/app/schemas/D_content_db.sql
+++ digital-content-predictor/backend/app/schemas/D_content_db.sql
@@ -49,3 +49,56 @@ CREATE TABLE PlanInterest (
     FOREIGN KEY (interest_id) REFERENCES Interest(interest_id) ON DELETE CASCADE
 );
 
+CREATE TABLE Recommendation (
+	recommendation_id INT NOT null AUTO_INCREMENT,
+	plan_id INT NOT NULL,
+	idea VARCHAR(255) NOT NULL,
+	platform varchar(16) not null,
+	title VARCHAR(50) NOT NULL,
+	performance VARCHAR(10) NOT NULL,
+	time VARCHAR(50) NOT null,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (recommendation_id),
+    FOREIGN KEY (plan_id) REFERENCES Plan(plan_id) ON DELETE cascade
+);		
+
+create table Caption (
+	caption_id INT NOT null AUTO_INCREMENT,
+	recommendation_id INT NOT null,
+	platform VARCHAR(16) NOT NULL,
+	caption TEXT NOT NULL,
+	hashtag VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,	
+	PRIMARY KEY (caption_id),
+	FOREIGN KEY (recommendation_id) REFERENCES Recommendation(recommendation_id) ON DELETE cascade
+);
+
+create table Platform (
+	platform_id INT NOT null AUTO_INCREMENT,
+	recommendation_id INT NOT null,
+	platform VARCHAR(16) NOT NULL,
+	prediction VARCHAR(16) NOT NULL,
+	PRIMARY KEY (platform_id),
+	FOREIGN KEY (recommendation_id) REFERENCES Recommendation(recommendation_id) ON DELETE cascade
+);
+
+create table Idea (
+	idea_id INT NOT null AUTO_INCREMENT,
+	recommendation_id INT NOT null,
+	idea_name VARCHAR(255) NOT NULL,
+	content_type VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (idea_id),
+	FOREIGN KEY (recommendation_id) REFERENCES Recommendation(recommendation_id) ON DELETE cascade
+);
+
+create table alternate (
+	alternate_id INT NOT null AUTO_INCREMENT,
+	idea_id INT NOT null,
+	idea_name VARCHAR(255) NOT NULL,
+	content_type VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (alternate_id),
+	FOREIGN KEY (idea_id) REFERENCES Idea(idea_id) ON DELETE cascade
+);
+
+
+
